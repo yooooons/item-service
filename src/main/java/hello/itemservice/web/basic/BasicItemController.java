d9e6a7b63a70c34ec5147033fa76aaf8d75a1693
YS <yooooonnf@gmail.com>
2022-02-18 18:46:23 +0900

    스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,8 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -29,6 +28,37 @@ public class BasicItemController {
         model.addAttribute("items", items);
         return "basic/items";
     }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);//모델에서 key,value저장하고 템플릿에서는 key값으로 찾고 추출한다.
+        return "basic/item";
+    }
+
+    //선호하는방시 methodMapping 을 이용한 구분
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+    @PostMapping("/add")
+    //form의 name으로 받아온다
+    public String save(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model
+    ) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+
+
 
     /*
     테스트용 데이터추가
